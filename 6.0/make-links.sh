6b8cbe88972637c5eb6499ae280ce4267465a2e9
Javier Burguete <jburguete@eead.csic.es>
2023-04-20 12:21:46 +0200

    Improved make links script to work in MacOS

--- 6.0/make-links.sh
+++ 6.0/make-links.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-if [ `uname -o` != 'Msys' ]; then
+if [ `uname -s` != 'Msys' ]; then
+  if [ `uname -s` != 'Darwin' ]; then
+    bin=bin/
+  fi
   ln -sf ../../../jb/jb src
-  ln -sf src/jb/bin/libjb-3.so
-  ln -sf src/jb/bin/libjbm-3.so
-  ln -sf src/jb/bin/libjbjson-3.so
-  ln -sf src/jb/bin/libjbbin-3.so
-  ln -sf src/jb/bin/libjbwin-3.so
+  ln -sf src/jb/${bin}libjb-3.so
+  ln -sf src/jb/${bin}libjbm-3.so
+  ln -sf src/jb/${bin}libjbjson-3.so
+  ln -sf src/jb/${bin}libjbbin-3.so
+  ln -sf src/jb/${bin}libjbwin-3.so
 else
   mkdir -p win64/bin
   ln -sf src/jb/bin/libjb-3.dll
