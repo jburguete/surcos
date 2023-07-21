4812feaf3c13c90234b81773e62a655d15f5d240
Javier Burguete <jburguete@eead.csic.es>
2023-07-21 09:12:38 +0200

    Corrected some bugs in Microsoft Windows distribution building script

--- 6.0/make-links.sh
+++ 6.0/make-links.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-if [ `uname -s` != 'Msys' ]; then
-  if [ `uname -s` != 'Darwin' ]; then
+if [ `uname -s` != 'Darwin' ]; then
     bin=bin/
-  fi
+fi
+if [ `uname -o` != 'Msys' ]; then
   ln -sf ../../../jb/jb src
   ln -sf src/jb/${bin}libjb-3.so
   ln -sf src/jb/${bin}libjbm-3.so
@@ -10,10 +10,10 @@ if [ `uname -s` != 'Msys' ]; then
   ln -sf src/jb/${bin}libjbbin-3.so
   ln -sf src/jb/${bin}libjbwin-3.so
 else
-  mkdir -p win64/bin
-  ln -sf src/jb/bin/libjb-3.dll
-  ln -sf src/jb/bin/libjbm-3.dll
-  ln -sf src/jb/bin/libjbjson-3.dll
-  ln -sf src/jb/bin/libjbbin-3.dll
-  ln -sf src/jb/bin/libjbwin-3.dll
+  bin=win64/bin
+  mkdir -p $bin
+  ln -sf ../../src/jb/bin/libjb-3.dll $bin
+  for i in m json bin win; do
+    ln -sf ../../src/jb/bin/libjb${i}-3.dll $bin
+  done
 fi
